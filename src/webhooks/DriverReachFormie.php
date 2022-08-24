d4f9fac645624818da21b690d1ed436f5061a84b
ConversioniaDev <61750899+ConversioniaDev@users.noreply.github.com>
2022-08-24 17:22:29 -0500

    Updated the optin logic to not default to YES

--- src/webhooks/DriverReachFormie.php
+++ src/webhooks/DriverReachFormie.php
@@ -105,7 +105,7 @@ class DriverReachFormie extends Webhook
                 'PrimaryPhone' => $this->_cleanPhone($data['cellPhone']),
                 'CommercialDriversLicense' => $data['cdlA'],
                 'LicenseClass' => $licenseClass,
-                'OptIn' => ($data['optIn'] ? 'Yes' : 'No'),
+                'OptIn' => ($data['optIn'] ?? null ?: 'No'),
             ],
         ];
 
