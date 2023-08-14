06afa13fc0ee73798b91807cb610b1b381b9b58b
Eric <elafontsee@gmail.com>
2023-08-14 11:54:05 -0500

    DEV-404 moved truck,job, and driver type evaluations into private function and removed redundant evaluations

--- src/exporters/GeosheetExporter.php
+++ src/exporters/GeosheetExporter.php
@@ -16,6 +16,11 @@ class GeosheetExporter extends ElementExporter
         return 'GeoSheet';
     }
 
+    private function getLabel($property, $fallbackProperty)
+    {
+        return !empty($property->value) ? $property->label : $fallbackProperty->label;
+    }
+
     public function export(ElementQueryInterface $query): array
     {
         $results = [];
@@ -27,33 +32,34 @@ class GeosheetExporter extends ElementExporter
         foreach ($query->each() as $element) {
 
             // Eager loaded defaultJobDescription
-            $fallback = $element->defaultJobDescription[0 ?? ''];
+            $fallback = $element->defaultJobDescription[0] ?? '';
 
             $campaign = !empty($element->assignedCampaign[0])
                 ? $element->assignedCampaign[0]
                 : '';
 
+
             /** @var Element $element */
             $results[] = [
                 'ID' => $element->id,
-                'Title' => $element->title ?? $fallback->title ?? '',
+                'Title' => $element->title ?? $fallback->title,
                 'Status' => ucfirst($element->status),
                 'URL' => $element->getUrl(),
-                'Trailer Type' => !empty($element->trailerType->value) ? $element->trailerType->label : $fallback->trailerType->label ?? '',
-                'Driver Type' => $element->driverType->label,
-                'Route Type' => $element->jobType->label,
-                'Job Type'  => $element->jobType->label,
-                'Assigned Campaign' => $campaign ?? '',
+                'Trailer Type' => $this->getLabel($element->trailerType, $fallback->trailerType),
+                'Driver Type' => $this->getLabel($element->driverType, $fallback->driverType),
+                'Route Type' => $this->getLabel($element->jobType, $fallback->jobType),
+                'Job Type' => $this->getLabel($element->jobType, $fallback->jobType),
+                'Assigned Campaign' => $campaign,
                 'Hiring Radius' => $element->hiringRadius ?? $fallback->hiringRadius,
-                'Google Jobs Title' => $element->googleJobsTitle ?? $fallback->googleJobsTitle ?? '',
-                'Ad Headline' => $element->adHeadline ?? $fallback->adHeadline ?? '',
+                'Google Jobs Title' => $element->googleJobsTitle ?? $fallback->googleJobsTitle,
+                'Ad Headline' => $element->adHeadline ?? $fallback->adHeadline,
                 'Location' => $element->location->formatted,
-                'City'  => $element->location->city,
+                'City' => $element->location->city,
                 'State' => $element->location->state,
-                'Zip'   => $element->location->zip,
+                'Zip' => $element->location->zip,
                 'Custom UTM Tag' => $element->customUtmTag ?? '',
                 'External Job ID' => $element->externalJobId ?? '',
-                'Pay' => $element->pay ?? $fallback->pay ?? '',
+                'Pay' => $element->pay ?? $fallback->pay,
             ];
         }
 
