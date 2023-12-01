151f8de464a9eaa080f39a0e99c5a96028b65efa
Eric <elafontsee@gmail.com>
2023-12-01 10:04:34 -0600

    removed reporter comments from example config

--- src/config.php
+++ src/config.php
@@ -2,8 +2,6 @@
 /**
  * LeadFlex plugin for Craft CMS 3.x
  *
- * CIA tool to build reports
- *
  * @link      conversionia.com
  * @copyright Copyright (c) 2023 Jeff Benusa
  */
@@ -14,7 +12,7 @@
  * This file exists only as a template for the LeadFlex settings.
  * It does nothing on its own.
  *
- * Don't edit this file, instead copy it to 'craft/config' as 'reporter.php'
+ * Don't edit this file, instead copy it to 'craft/config' as 'leadflex.php'
  * and make your changes there to override default settings.
  *
  * Once copied to 'craft/config', this file will be multi-environment aware as
