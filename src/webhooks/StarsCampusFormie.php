e17af1440d77a93d996967b556e96b6271858d92
Jeffrey Benusa <jbenusa@conversionia.com>
2023-12-26 11:46:29 -0600

    Sending value for LeadOriginCode as well as Campaign.

--- src/webhooks/StarsCampusFormie.php
+++ src/webhooks/StarsCampusFormie.php
@@ -112,6 +112,7 @@ class StarsCampusFormie extends Webhook
                 'CompanyId' => trim($data['atsCompanyId']),
                 'DriverId' => $driverId,
                 'AppReferrer' => trim($data['referrerValue']),
+                'LeadOriginCode' => trim($data['referrerValue']),
                 'GivenName' => trim($data['firstName']),
                 'FamilyName' => trim($data['lastName']),
                 'Municipality' => trim($data['city']),
